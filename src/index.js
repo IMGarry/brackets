778e7f625a33bdefc7a8a6c540be797e2f74ecfd
Igor Marchenko <igor.ms83@gmail.com>
2021-03-15 18:57:44 +0300

    docs: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arr = bracketsConfig.map((it) => it.join(''));
+  for (let i = 0; i <= arr.length; i++) {
+    if (str.indexOf(arr[i]) > -1) {
+      str = str.replace(arr[i], '');
+      i = -1;
+    }
+  }
+  return !str.length;
 }
